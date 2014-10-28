93933b68bba2fa8d414d4104bf24116735163b24
amisukanta <amisukanta02@gmail.com>
2014-10-28 12:19:19 +0100

    heroku 1

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,6 +1,6 @@
 require 'rufus-scheduler'
 
 #$scheduler = Rufus::Scheduler.new
-@crons = Schedule.set_cron
+#@crons = Schedule.set_cron
 
 Rufus::Scheduler.singleton.cron("20 12 * * *") { Feed.pull_tweets }
