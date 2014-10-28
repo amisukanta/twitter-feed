7dea686050a928b0fd4213e98ec53b88a8223477
amisukanta <amisukanta02@gmail.com>
2014-10-28 12:07:30 +0100

    initializer modified

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,6 +1,6 @@
 require 'rufus-scheduler'
 
 #$scheduler = Rufus::Scheduler.new
-@crons = Schedule.set_cron
+#@crons = Schedule.set_cron
 
-Rufus::Scheduler.singleton.cron("0 #{@crons} * * *") { Feed.pull_tweets }
+#Rufus::Scheduler.singleton.cron("0 #{@crons} * * *") { Feed.pull_tweets }
