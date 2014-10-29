dfdf96153a7284a80a8a95278eb3483fcd2c3cea
amisukanta <amisukanta02@gmail.com>
2014-10-29 17:39:08 +0100

    refactor fed model and paginate in view

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -2,8 +2,7 @@ class FeedsController < ApplicationController
   
   def index
   	@crons = Schedule.set_cron
-
-    Rufus::Scheduler.singleton.cron("0 #{@crons} * * *") { Feed.pull_tweets }
+	Rufus::Scheduler.singleton.cron("37 #{@crons} * * *") { Feed.pull_tweets }
   	@feeds = Feed.paginate(:page => params[:page], :per_page => 500).order('created_at DESC')
   end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -2,14 +2,14 @@ class Feed < ActiveRecord::Base
 
 
 	def self.pull_tweets
-    $client.search("#ukraine", since_id: maximum(:tweet_id), count: 500, :result_type => "recent").take(500).each do |tweet|
-      unless exists?(tweet_id: tweet.id)
+    $client.search("#ukraine", count: 500, :result_type => "recent").take(500).each do |tweet|
+      
         create!(
           tweet_id: tweet.id,
           content: tweet.text,
-          screen_name: tweet.user.screen_name,
+          screen_name: tweet.user.screen_name
         )
-      end
+      
     end
   end
   
